88a8e1d27776c66be8aa76a25e8b4d443eb71e4e
Muhammed Emre Ağda <119903051+muhammedemreagda@users.noreply.github.com>
2025-04-01 04:42:41 +0300

    Create dilbelirleme.c

---
+++ dilbelirleme.c
@@ -0,0 +1,1 @@
+//Önceden yüklediğim dosyaların isimlerinde ".c" uzantısı kullanmadığımdan Github projelerin c projesi olduğunu okuyamıyor. Bu dosyayı oluşturarak proje dilinin c olarak gözükmesini sağladım.  :)
